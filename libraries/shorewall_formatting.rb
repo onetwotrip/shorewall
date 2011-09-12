e7c7e7983d34bc4a8df609c351b6e2274eb388e0
denz <denz@twiket.com>
2011-09-13 00:26:21 +0400

    shorewall recipe automatic nested zone ordering

--- libraries/shorewall_formatting.rb
+++ libraries/shorewall_formatting.rb
@@ -1,9 +1,24 @@
+#
+# arrange shorewall.zones array in the right order
+#
 def order_zones()
   zones = node[:shorewall][:zones].clone
   node[:shorewall][:zones].clear
-  node[:shorewall][:zones_order].split(",").each do |zone|
-    # handle simple or nested zones
-    node.override[:shorewall][:zones] << zones.select { |el| el[:zone] == zone or el[:zone] =~ /#{zone}:/ }[0]
+  ordered = node[:shorewall][:zones_order].split(",")
+  # put zones in the specified order
+  ordered.each do |s_zone|
+    zone = zones.select { |el| el[:zone] == s_zone or el[:zone] =~ /#{s_zone}:/ }[0]
+    node.override[:shorewall][:zones] << zones.delete(zone)
+  end
+  return if zones.empty? == true
+  # more zones left which we include automatically (but if only they are nested)
+  zones.each do |zh|
+    if ! zh[:zone].include?(':')
+      Chef::Log.error("shorewall zones_order must include zone #{zh[:zone]}") && raise
+    end
+    parent_zone = zh[:zone].split(":")[1]
+    i = node[:shorewall][:zones].index { |el| el[:zone] == parent_zone }
+    node[:shorewall][:zones].insert(i+1, zh)
   end
 end
 
